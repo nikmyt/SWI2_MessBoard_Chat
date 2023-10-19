3dea8cc4f89e6ac76424962206ac3fa0228d62e6
Slamak66 <79204343+Slamak66@users.noreply.github.com>
2023-10-19 16:51:36 +0200

    rabbitmq
    basic implementation of rabbitmq

---
+++ backend/src/main/java/cz/osu/java/messboardapp/service/Receiver.java
@@ -0,0 +1,21 @@
+package cz.osu.java.messboardapp.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CountDownLatch;
+
+@Service
+public class Receiver {
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    public void receiveMessage (String message)
+    {
+        System.out.println("Received <" + message + ">");
+        latch.countDown();
+    }
+
+    public CountDownLatch getLatch()
+    {
+        return latch;
+    }
+}
