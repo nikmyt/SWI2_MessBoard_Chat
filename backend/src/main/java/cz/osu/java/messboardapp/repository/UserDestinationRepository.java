98630f7e92c88e22c9e549e7d8ba41c8aa54fe4e
Slamak66 <79204343+Slamak66@users.noreply.github.com>
2024-01-26 16:38:14 +0100

    fixed crash
    uhhh I dont know how tho

--- backend/src/main/java/cz/osu/java/messboardapp/repository/UserDestinationRepository.java
+++ backend/src/main/java/cz/osu/java/messboardapp/repository/UserDestinationRepository.java
@@ -2,10 +2,14 @@ package cz.osu.java.messboardapp.repository;
 
 import cz.osu.java.messboardapp.model.UserDestination;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface UserDestinationRepository extends JpaRepository<UserDestination, Integer> {
+
+    // Custom query method to find all destinationIds for a specific userId
+    @Query("SELECT ud.id.destinationId FROM UserDestination ud WHERE ud.id.userId = :userId")
     List<Long> findDestinationIdsByUserId(@Param("userId") Long userId);
 }
